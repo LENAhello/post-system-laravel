fbf9197c486b458fccc548851d080e5451fb2fb0
LeNa24Shi <lena.shihab@gritacademy.se>
2025-07-25 19:05:43 +0200

    Add buttons to delete and edits posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,6 +7,21 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>index page</h1>
+    <h1>All Posts</h1>
+
+    <a href="{{ route('posts.create') }}">Create new post</a>
+    @foreach($posts as $post)
+    <div>
+        <h2>{{ $post->title }}</h2>
+        <p>{{ $post->body }}</p>
+        <a href="{{ route('posts.edit', $post->id) }}">Edit</a>
+        <form action="{{ route('posts.destroy', $post->id) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit" onclick="return confirm('Are you sure you want to delete this post?')">Delete</button>
+        </form>
+    </div>
+    @endforeach
+
 </body>
 </html>
